c485245cf1dd5ffb1b81725b050c29e022d41b05
Bagus Ahlul <bagus.ahlul.ft21@mail.umy.ac.id>
2023-12-28 19:47:53 +0700

    add function HomeScreen

--- app/src/main/java/com/example/datainternet/ui/theme/home/viewmodel/HomeScreen.kt
+++ app/src/main/java/com/example/datainternet/ui/theme/home/viewmodel/HomeScreen.kt
@@ -18,6 +18,7 @@ import androidx.compose.material.icons.filled.Phone
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
@@ -27,8 +28,11 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.datainternet.R
 import com.example.datainternet.model.Kontak
+import com.example.datainternet.navigation.DestinasiNavigasi
+import com.example.datainternet.ui.theme.PenyediaViewModel
 
 
 @Composable
@@ -152,3 +156,17 @@ fun KontakCard(
 
     }
 }
+
+object DestinasiHome : DestinasiNavigasi {
+    override val route = "home"
+    override val titleRes = "Kontak"
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun HomeScreen(
+    navigateToItemEntry: () -> Unit,
+    modifier: Modifier = Modifier,
+    onDetailClick: (Int) -> Unit = {},
+    viewModel: HomeViewModel = viewModel(factory = PenyediaViewModel.Factory)
+) {}
