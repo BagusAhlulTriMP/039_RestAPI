27a2abd1451895b9413bd54ff430b567266e4eb7
Bagus Ahlul <bagus.ahlul.ft21@mail.umy.ac.id>
2023-12-28 20:09:00 +0700

    menambahkan spasi

--- app/src/main/java/com/example/datainternet/ui/theme/home/viewmodel/HomeScreen.kt
+++ app/src/main/java/com/example/datainternet/ui/theme/home/viewmodel/HomeScreen.kt
@@ -15,6 +15,7 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Delete
 import androidx.compose.material.icons.filled.Phone
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
@@ -22,6 +23,7 @@ import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -159,6 +161,14 @@ fun KontakCard(
                 text = kontak.email,
                 style = MaterialTheme.typography.titleMedium
             )
+            Spacer(modifier.weight(1f))
+            IconButton(onClick = { onDeleteClick(kontak) }) {
+                Icon(
+                    imageVector = Icons.Default.Delete,
+                    contentDescription = null,
+                )
+
+            }
         }
 
     }
