0c9f7d0258636e03ff9706abf872e319c5820a46
Bagus Ahlul <bagus.ahlul.ft21@mail.umy.ac.id>
2023-12-28 18:05:04 +0700

    add data class InsertUiState

--- app/src/main/java/com/example/datainternet/ui/theme/kontak/screen/viewmodel/InsertViewModel.kt
+++ app/src/main/java/com/example/datainternet/ui/theme/kontak/screen/viewmodel/InsertViewModel.kt
@@ -1,8 +1,19 @@
 package com.example.datainternet.ui.theme.kontak.screen.viewmodel
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 import com.example.datainternet.repository.KontakRepository
 
 class InsertViewModel(private val kontakRepository: KontakRepository) : ViewModel() {
+var insertKontakState by mutableStateOf(InsertUiState())
+    private set
+    fun updateInsertKontakState(insertUiEvent: InsertUiEvent) {
 
+    }
 }
+
+data class InsertUiState(
+    val insertUiEvent: InsertUiEvent = InsertUiEvent(),
+)
