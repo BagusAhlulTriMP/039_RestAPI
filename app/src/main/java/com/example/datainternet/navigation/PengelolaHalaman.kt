88dd76f49c8dee1737f5a4446ed4b4b3054e3cb7
Bagus Ahlul <bagus.ahlul.ft21@mail.umy.ac.id>
2023-12-29 06:57:31 +0700

    add function PengelolaHalaman

--- app/src/main/java/com/example/datainternet/navigation/PengelolaHalaman.kt
+++ app/src/main/java/com/example/datainternet/navigation/PengelolaHalaman.kt
@@ -1,6 +1,38 @@
 package com.example.datainternet.navigation
 
 import androidx.compose.runtime.Composable
+import com.example.datainternet.ui.theme.home.viewmodel.DestinasiHome
+import com.example.datainternet.ui.theme.home.viewmodel.HomeScreen
+import com.example.datainternet.ui.theme.kontak.screen.viewmodel.DestinasiEntry
+import com.example.datainternet.ui.theme.kontak.screen.viewmodel.EntryKontakScreen
 
 @Composable
-fun PengelolaHalaman
+fun PengelolaHalaman(navController: NavHostController = rememberNavController()) {
+
+    NavHost(
+        navController = navController,
+        startDestination = DestinasiHome.route,
+        modifier = Modifier,
+    ) {
+
+        composable(DestinasiHome.route) {
+            HomeScreen(navigateToItemEntry = {
+                navController.navigate(DestinasiEntry.route)
+            },
+                onDetailClick = {
+
+                })
+        }
+        composable(DestinasiEntry.route) {
+            EntryKontakScreen(navigateBack = {
+                navController.navigate(
+                    DestinasiHome.route
+                ) {
+                    popUpTo(DestinasiHome.route) {
+                        inclusive = true
+                    }
+                }
+            })
+        }
+    }
+}
