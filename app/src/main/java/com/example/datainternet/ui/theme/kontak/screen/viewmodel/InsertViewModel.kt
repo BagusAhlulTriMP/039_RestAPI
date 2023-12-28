ae7061b1abd34fc87e5623476f61ba4bf34105fc
Bagus Ahlul <bagus.ahlul.ft21@mail.umy.ac.id>
2023-12-28 18:00:15 +0700

    add class InsertViewModel

---
+++ app/src/main/java/com/example/datainternet/ui/theme/kontak/screen/viewmodel/InsertViewModel.kt
@@ -0,0 +1,8 @@
+package com.example.datainternet.ui.theme.kontak.screen.viewmodel
+
+import androidx.lifecycle.ViewModel
+import com.example.datainternet.repository.KontakRepository
+
+class InsertViewModel(private val kontakRepository: KontakRepository) : ViewModel() {
+
+}
