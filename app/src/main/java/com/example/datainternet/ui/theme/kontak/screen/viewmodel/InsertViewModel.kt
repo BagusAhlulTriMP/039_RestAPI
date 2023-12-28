06076aaecd14f885a8a1055ea56a9347b1bac058
Bagus Ahlul <bagus.ahlul.ft21@mail.umy.ac.id>
2023-12-28 18:19:09 +0700

    menyimpan data kontak ke dalam repositori

--- app/src/main/java/com/example/datainternet/ui/theme/kontak/screen/viewmodel/InsertViewModel.kt
+++ app/src/main/java/com/example/datainternet/ui/theme/kontak/screen/viewmodel/InsertViewModel.kt
@@ -4,14 +4,26 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.datainternet.model.Kontak
 import com.example.datainternet.repository.KontakRepository
+import kotlinx.coroutines.launch
 
 class InsertViewModel(private val kontakRepository: KontakRepository) : ViewModel() {
-var insertKontakState by mutableStateOf(InsertUiState())
-    private set
+    var insertKontakState by mutableStateOf(InsertUiState())
+        private set
     fun updateInsertKontakState(insertUiEvent: InsertUiEvent) {
+        insertKontakState = InsertUiState(insertUiEvent = insertUiEvent)
+    }
 
+    suspend fun  insertKontak() {
+        viewModelScope.launch {
+            try {
+                kontakRepository.insertKontak(insertKontakState.insertUiEvent.toKontak())
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+        }
     }
 }
 
@@ -31,6 +43,9 @@ fun InsertUiEvent.toKontak(): Kontak = Kontak(
     nama = nama,
     email = email,
     nohp = nohp,
+)
+fun Kontak.toUiStateKontak(): InsertUiState = InsertUiState(
+    insertUiEvent = toInsertUiEvent(),
 )
 
 fun Kontak.toInsertUiEvent(): InsertUiEvent = InsertUiEvent(
