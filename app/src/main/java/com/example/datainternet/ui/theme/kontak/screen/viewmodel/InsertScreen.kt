8dc1881727a3f9f1df782f84000f0b6a58f34e38
Bagus Ahlul <bagus.ahlul.ft21@mail.umy.ac.id>
2023-12-28 18:35:53 +0700

    menambahkan input teks nama

--- app/src/main/java/com/example/datainternet/ui/theme/kontak/screen/viewmodel/InsertScreen.kt
+++ app/src/main/java/com/example/datainternet/ui/theme/kontak/screen/viewmodel/InsertScreen.kt
@@ -1,8 +1,14 @@
 package com.example.datainternet.ui.theme.kontak.screen.viewmodel
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -11,4 +17,17 @@ fun FormInputSiswa(
     modifier: Modifier = Modifier,
     onValueChange: (InsertUiEvent) -> Unit = {},
     enabled: Boolean = true
-) {}
+) {
+    Column(
+        modifier = modifier,
+        verticalArrangement = Arrangement.spacedBy(12.dp)
+    ) {
+        OutlinedTextField(
+            value = insertUiEvent.nama,
+            onValueChange = { onValueChange(insertUiEvent.copy(nama = it)) },
+            label = { Text("Nama") },
+            modifier = Modifier.fillMaxWidth(),
+            enabled = enabled,
+            singleLine = true)
+    }
+}
