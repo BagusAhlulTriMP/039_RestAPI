4ae1953a095d1bcafb792641d15c32c97107b1d9
Bagus Ahlul <bagus.ahlul.ft21@mail.umy.ac.id>
2023-12-28 18:44:09 +0700

    menambahkan keyboardoptions pada nohp

--- app/src/main/java/com/example/datainternet/ui/theme/kontak/screen/viewmodel/InsertScreen.kt
+++ app/src/main/java/com/example/datainternet/ui/theme/kontak/screen/viewmodel/InsertScreen.kt
@@ -4,12 +4,14 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Divider
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -45,6 +47,7 @@ fun FormInputSiswa(
         OutlinedTextField(
             value = insertUiEvent.nohp,
             onValueChange = { onValueChange(insertUiEvent.copy(nohp = it)) },
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
             label = { Text("No HP") },
             modifier = Modifier.fillMaxWidth(),
             enabled = enabled,
