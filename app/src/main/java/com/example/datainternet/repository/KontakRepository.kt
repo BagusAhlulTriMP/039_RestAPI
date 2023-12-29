e4827845a0f4e75cc26c9da993b37cd6b75de5fb
Bagus Ahlul <bagus.ahlul.ft21@mail.umy.ac.id>
2023-12-29 07:14:30 +0700

    add suspend function deletekontak dan insertkontak

--- app/src/main/java/com/example/datainternet/repository/KontakRepository.kt
+++ app/src/main/java/com/example/datainternet/repository/KontakRepository.kt
@@ -6,6 +6,8 @@ import com.example.datainternet.service_api.KontakService
 interface KontakRepository {
     /** Fetches list of Kontak from kontakApi */
     suspend fun getKontak(): List<Kontak>
+    suspend fun deleteKontak(id: Int): Boolean
+    suspend fun insertKontak(kontak: Kontak): Boolean
 }
 
 class NetworkKontakRepository(
@@ -13,4 +15,12 @@ class NetworkKontakRepository(
 ) : KontakRepository {
     /** Fetches list of Kontak from kontakApi*/
     override suspend fun getKontak(): List<Kontak> = kontakApiService.getKontak()
+    override suspend fun deleteKontak(id: Int): Boolean {
+        val response = kontakApiService.deleteKontak(id)
+        return response.isSuccessful
+    }
+    override suspend fun insertKontak(kontak: Kontak): Boolean {
+        val response = kontakApiService.insertKontak(kontak)
+        return response.isSuccessful
+    }
 }
